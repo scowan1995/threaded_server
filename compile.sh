a235b05069f97635f9d2bedb8a11343d734b5198
root <root@LocalDeliveryOnly>
2016-11-13 15:17:36 +0000

    cd into the correct folder

--- compile.sh
+++ compile.sh
@@ -2,7 +2,7 @@
 
 wget https://www.python.org/ftp/python/3.5.2/Python-3.5.2rc1.tgz 
 tar xvzf Python-3.5.2rc1.tgz 
-cd Python-3.5.2rc1.tgz
+cd Python-3.5.2rc1
 ./configure
 make
 make altinstall prefix=~/local
