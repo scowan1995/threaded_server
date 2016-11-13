c81c63d6de4efd1d8f1d20f97e52503805003760
root <root@LocalDeliveryOnly>
2016-11-13 15:34:57 +0000

    faster compile

--- compile.sh
+++ compile.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-
-wget https://www.python.org/ftp/python/3.5.2/Python-3.5.2rc1.tgz 
-tar xvzf Python-3.5.2rc1.tgz 
-cd Python-3.5.2rc1
-./configure
-make
-make altinstall prefix=~/local
-ln -s ~/local/bin/python3.5.2 ~/local/bin/python
+apt-get update
+apt-get install python3
+alias python=python3
