0c127bf8f8cd000b2dcf8a490327c73eb184bbfa
scowan1995 <scowan@tcd.ie>
2016-11-13 15:04:38 +0000

    bash scripts to install and configure python 3, and to run the server with a portnumber argument.

---
+++ compile.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+wget https://www.python.org/ftp/python/3.5.2/Python-3.5.2rc1.tgz 
+tar xvzf Python-3.5.2rc1.tgz 
+cd Python-3.5.2rc1.tgz
+./configure
+make
+make altinstall prefix=~/local
+ln -s ~/local/bin/python3.5.2 ~/local/bin/python
